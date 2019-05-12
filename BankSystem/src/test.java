0d58238434d29c22dda4f3cafe17501fc2b99abf
ZengzeWang <35391259+ZengzeWang@users.noreply.github.com>
2019-05-12 14:34:13 +0800

    Add files via upload

---
+++ BankSystem/src/test.java
@@ -0,0 +1,11 @@
+public class test {
+	/**
+	 * Main method, used to run the program.
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+
+		BankControl b = new BankControl();
+	}
+}
